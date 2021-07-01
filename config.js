28b48f2b77106f181d2b0bf674c0b10d9e29a5e6
huy1401 <86797429+huy1401@users.noreply.github.com>
2021-07-02 00:42:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Em yêu à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnIntro: 'Anh có bất ngờ cho em nè',
+    title: 'Anh yêu em rất rất lâu rồi á, yêu điên dại luôn 😙',
+    desc: 'Em có yêu anh hong á? ',
+    btnYes: 'Yêu anh lắm luôn, Darling <33',
+    btnNo: 'Đéo, Anh bị điên à à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    btnReply: 'Anh lắm trò quá đó <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều lắm luôn 😘😘',
+    messDesc: 'Khi nào hết dịch tụi mình sẽ đi du lịch cùng nhau nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    messLink: 'https://www.facebook.com/profile.php?id=100065309171127'
